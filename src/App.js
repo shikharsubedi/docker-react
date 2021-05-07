32097b1320d6097fa16115022144c0219824a463
shikhar <shikhar@bstock.com>
2021-05-06 20:19:49 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi there! This is shikhar. the volumes is working. It is working!!
         </p>
         <a
           className="App-link"
--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,13 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+test('renders learn react link 2', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+test('renders learn react link 3', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
